0b5b3444485f7f8e77ff06f340fff2e3349393f2
laowang1992 <wangpf0608@126.com>
2025-01-16 11:31:39 +0800

    update parameter name of emapplot for enrichment (Bioconductor version 3.20)

--- 05.ORA/enrich.R
+++ 05.ORA/enrich.R
@@ -87,7 +87,15 @@ write_tsv(de_ego_df, paste(out_prefix, "GO_result", "txt", sep = "."))
 ## GO绘图
 # 
 if (nrow(de_ego_df)>0) {
-  GO_emap <- emapplot(pairwise_termsim(de_ego), cex.params = list(category_label = .8, line = .5)) + 
+  #GO_emap <- emapplot(pairwise_termsim(de_ego), cex_label_category=.8, cex_line=.5) + 
+  #  scale_fill_continuous(low = "#e06663", high = "#327eba", name = "p.adjust",
+  #                        guide = guide_colorbar(reverse = TRUE, order=1), trans='log10')
+  #GO_emap <- emapplot(pairwise_termsim(de_ego), cex.params = list(category_label = .8, line = .5)) + 
+  #  scale_fill_continuous(low = "#e06663", high = "#327eba", name = "p.adjust",
+  #                        guide = guide_colorbar(reverse = TRUE, order=1), trans='log10')
+  # Bioconductor 升级到 version 3.20 以后，参数名称修改，
+  # 为啥要修改参数名啊，功能难道有不同吗？用了好多年老改参数很不方便啊
+  GO_emap <- emapplot(pairwise_termsim(de_ego), size_category=.8, size_edge=.5) + 
     scale_fill_continuous(low = "#e06663", high = "#327eba", name = "p.adjust",
                           guide = guide_colorbar(reverse = TRUE, order=1), trans='log10')
   ggsave(GO_emap, filename = paste(out_prefix, "GO_emapplot.pdf", sep = "."), width = 10, height = 9)
@@ -134,7 +142,7 @@ write_tsv(de_ekp_df, paste(out_prefix, "KEGG_result", "txt", sep = "."))
 
 # KEGG绘图
 if (nrow(de_ekp_df)>0) {
-  KEGG_emap <- emapplot(pairwise_termsim(de_ekp), cex.params = list(category_label = .8, line = .5)) + 
+  KEGG_emap <- emapplot(pairwise_termsim(de_ekp), size_category=.8, size_edge=.5) + 
     scale_fill_continuous(low = "#e06663", high = "#327eba", name = "p.adjust",
                           guide = guide_colorbar(reverse = TRUE, order=1), trans='log10')
   ggsave(KEGG_emap, filename = paste(out_prefix, "KEGG_emapplot.pdf", sep = "."), width = 10, height = 9)
